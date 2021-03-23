185e24d8cff22d3e5511f470744f521c49f8bd0b
nathan hains <nathanhains14@yahoo.com>
2021-03-23 12:30:01 -0400

    User exercises - Added functionality to route to exercise show page from links of all users exercises shown in user's home page.

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -4,5 +4,18 @@ class ExercisesController < ApplicationController
     end
 
     post '/exercises' do
+        if params[:name] != "" && params[:muscle_group] != "" && params[:sets] != "" && params[:reps] != "" && params[:description] != ""
+            @exercise = Exercise.create(params)
+            @exercise.user_id = session[:user_id]
+            redirect "/exercises/#{@exercise.id}"
+        else
+            #include failure message
+            redirect '/exercises/new'
+        end
+    end
+
+    get '/exercises/:id' do
+        @exercise = Exercise.find_by(id: params[:id])
+        erb :'/exercises/show'
     end
 end
