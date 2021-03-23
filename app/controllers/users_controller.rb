0b69c8313dcfef9a0a2b0c447cd8a00042f8aee9
nathan hains <nathanhains14@yahoo.com>
2021-03-22 22:55:50 -0400

    Login.erb - created anerb file that will render the login page to pass user input to create a session.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+    #route usage: to render login form
     get '/login' do
         erb :'/users/login'
+    end
+
+    #route usage: to receive login form & create a session(find user, log user in)
+    post '/login' do
+
     end
 
     get '/signup' do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 #create 2 users
-nate = User.create(name: "Nate", email: "natehains@yahoo.com", password: "password")
-raven = User.create(name: "Raven", email: "ravenmccoy@yahoo.com", password: "password")
+nate = User.create(username: "Nate", email: "natehains@yahoo.com", password: "password")
+raven = User.create(username: "Raven", email: "ravenmccoy@yahoo.com", password: "password")
 
 Exercise.create(name: "Bicep Dumbell Curl", description: "Curl dumbells starting with arms perpindicular to the floor and lift to a 45 degree angle. Repeat.", reps:"8-12", sets:"3-4", muscle_group: "Bicep", user_id: nate.id)
 Exercise.create(name: "Bicep Barbell Curl", description: "Curl barbell starting with arms perpindicular to the floor and lift to a 45 degree angle. Repeat.", reps:"8-12", sets:"3-4", muscle_group: "Bicep", user_id: raven.id)
