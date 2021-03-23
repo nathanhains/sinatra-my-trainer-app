e04bbb1ef9a4424ee01316cbaf15f6efdaec0fbe
nathan hains <nathanhains14@yahoo.com>
2021-03-22 23:37:35 -0400

    Helper methods - created methods to determin the current user & to return true/false whether the user is logged in or not.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,16 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+    
+    def logged_in?
+      # double bang operator to return true
+      !!current_user
+    end
+
+    def current_user
+      #reduces database calls(memoization) if its already populated, wont get database again
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+
 end
