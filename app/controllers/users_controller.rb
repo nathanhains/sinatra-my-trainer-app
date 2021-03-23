a7b9965eb42b8af7d037c6f2cf5c0ac7462e7c10
nathan hains <nathanhains14@yahoo.com>
2021-03-22 23:27:36 -0400

    User authentication- added functionality for user login using sessions for user identification.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,22 @@ class UsersController < ApplicationController
 
     #route usage: to receive login form & create a session(find user, log user in)
     post '/login' do
+        @user = User.find_by(username: params[:username])
+        #is the user in the database?
+        if @user.authenticate(params[:password])
+            #create session
+            session[:user_id] = @user.id
+            redirect "users/#{@user.id}"
+        else
+
+        end
 
     end
 
     get '/signup' do
+    end
+
+    get '/users/:id' do
+        "Helllooooo"
     end
 end
