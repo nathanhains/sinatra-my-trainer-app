2a259adc473e08af4d652c2acc459ed6049f3f29
nathan hains <nathanhains14@yahoo.com>
2021-03-22 21:44:58 -0400

    Exercises - created table and migration for exercises. Relationships - established has_many/belongs_to relationship between users and exercises.

--- db/migrate/20210323011433_create_users.rb
+++ db/migrate/20210323011433_create_users.rb
@@ -4,6 +4,8 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :name
       t.string :email
       t.string :password_digest
+
+      t.timestamps null: false
     end
   end
 end
---
+++ db/migrate/20210323013624_create_exercises.rb
@@ -0,0 +1,13 @@
+class CreateExercises < ActiveRecord::Migration
+  def change
+    create_table :exercises do |t|
+      t.string :name
+      t.string :description
+      t.string :reps
+      t.string :sets
+      t.string :muscle_group
+      t.integer :user_id
+      t.timestamps null: false
+    end
+  end
+end
