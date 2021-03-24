d09bfc14132172632b60d1606d4eac7f62ea3022
nathan hains <nathanhains14@yahoo.com>
2021-03-24 17:16:07 -0400

    Added has many/has many through relationship for likes feature bewteen users and exercises.

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,4 @@
 class Exercise < ActiveRecord::Base
     belongs_to :user
+    has_many :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User <ActiveRecord::Base
     has_secure_password
     has_many :exercises
+    has_many :likes, through: :exercises
     include Slugifiable
     extend Slugifiable::ClassMethods
 end
