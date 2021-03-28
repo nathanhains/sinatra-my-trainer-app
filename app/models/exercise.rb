38c6f52ed28419ec04e93c207229dea1217293c1
nathan hains <nathanhains14@yahoo.com>
2021-03-28 16:46:45 -0400

    Top feature - Ironed out a bug that was producing internal server errors.

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,9 @@
 class Exercise < ActiveRecord::Base
     belongs_to :user
     has_many :likes
+    def self.find_case_insensitive(name)
+        Exercise.all.find do |exercise|
+            name.downcase == exercise.name.downcase
+        end
+    end
 end
